8defa8e0090c51dc7ddfbb0eb256fa8fc53b4c00
kerim <kerim@DESKTOP-O7UU4I3>
2022-01-19 11:40:27 +0300

    Add project files.

---
+++ EducationFirst/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace EducationFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            object a = 50;
+            object b = "aaaaa";
+            float piSayisi = 3.14F;
+            double piDouble = 3.1457687d;
+            bool control = true;
+            char words = 'a';
+            string hello= "Hello";
+            sbyte eksideger = -128;
+            byte deger = 56; //255 e kadar sayılar
+            uint sayi3 = 1;//4 byte pozitif sayılar
+
+
+
+            Console.WriteLine("Değerler"+piSayisi);
+            Console.WriteLine("Değerler"+ piDouble);
+            Console.WriteLine("Değerler"+ control);
+            Console.WriteLine("Değerler"+ words);
+            Console.WriteLine("Değerler"+ hello);
+            Console.WriteLine("Değerler" + eksideger);
+            Console.WriteLine("Değerler" + deger);
+            Console.WriteLine("Değerler" + sayi3);
+
+
+
+
+        }
+    }
+}
